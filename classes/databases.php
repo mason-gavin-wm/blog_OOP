c9df1dbc00c76671229c8c98fe105bb9b6346091
Gavin Mason <session1@START-Coding-MBP-15.local>
2016-10-03 13:32:26 -0700

    It works now, got more off the direct instructions, I still need to add Left join and overload

---
+++ classes/databases.php
@@ -0,0 +1,92 @@
+<?php
+
+class Database {
+
+    private $host = 'localhost';
+    private $user = 'root';
+    private $pass = 'root';
+    private $dbname = 'blog_oop';
+
+    private $dbh;
+    private $error;
+    private $stmt;
+
+
+    public function __construct(){
+
+        $dsn ='mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+
+        $options = array(
+
+            PDO::ATTR_PERSISTENT    => true,
+            PDO::ATTR_ERRMODE   => PDO::ERRMODE_EXCEPTION
+        );
+
+        try{
+
+            $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+
+        }   catch(PDOException $e){
+
+            $this->error = $e->getMessage();
+
+            echo $this->error;
+        }
+
+    }//-----------End of function __construct-----------//
+
+    public function query($query){
+
+        $this->stmt = $this->dbh->prepare($query);
+    }//-----------End of function query-----------//
+
+
+    public function bind($param, $value, $type = null){
+
+        if(is_null($type)){
+
+            switch(true){
+
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+
+        }
+
+
+        $this->stmt->bindValue($param, $value, $type);
+
+    }//-----------End of function bind-----------//
+
+
+    public function execute(){
+
+        return $this->stmt->execute();
+    }
+
+    public function lastInsertID(){
+
+        return $this->dbh->lastInsertId();
+    }
+
+    public function resultset(){
+        $this->execute();
+
+        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+}//-----------End of class Database-----------//
+
+?>
--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: session1
- * Date: 9/28/16
- * Time: 1:57 PM
- */
